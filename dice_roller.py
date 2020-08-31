9a9b1ab85ff1605afb0999288f151975a1b739b3
Phillip Tuckwell <phillip.tuckwell@gmail.com>
2020-09-01 08:54:59 +1000

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,18 @@
+
+    
 def main():
-  print('You rolled a die')
+    import random
+    dice_rolls = 2
+    dice_sum = 0
+    for i in range(0, dice_rolls):
+    
+        roll = random.randint(1,6)
+        #Python's f-string functionality. 
+        #By putting an f right before the quotes of the string you're printing, 
+        #you can then print variables within the string
+        dice_sum = dice_sum + roll
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
